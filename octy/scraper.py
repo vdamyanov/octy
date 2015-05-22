987adcf8e9e28cd7d67fa672b15aff334db7458b
Vasil Damyanov <vasil_damyanov@yahoo.com>
2015-05-22 11:15:21 -0400

    added requirement for beautifulsoup4 and wrote a small test program

---
+++ octy/scraper.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'www.pythonforbeginners.com'
+
+r = requests.get('http://'+url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+  print link.get('href')
